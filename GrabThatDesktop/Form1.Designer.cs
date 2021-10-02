c63118a96326c3e5d6e3d6f481526f5be9abea55
D S <saougos@hotmail.com>
2021-10-02 23:28:11 +0300

    will this work?

--- GrabThatDesktop/Form1.Designer.cs
+++ GrabThatDesktop/Form1.Designer.cs
@@ -29,12 +29,108 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startRecording = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopRecording = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.desktopSelector = new System.Windows.Forms.ToolStripMenuItem();
+            this.desktopSelectorStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.desktopSelector,
+            this.toolStripSeparator1,
+            this.stopRecording,
+            this.startRecording,
+            this.toolStripSeparator2,
+            this.exitMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(156, 126);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // startRecording
+            // 
+            this.startRecording.Name = "startRecording";
+            this.startRecording.Size = new System.Drawing.Size(155, 22);
+            this.startRecording.Text = "Start Recording";
+            // 
+            // stopRecording
+            // 
+            this.stopRecording.Name = "stopRecording";
+            this.stopRecording.Size = new System.Drawing.Size(155, 22);
+            this.stopRecording.Text = "Stop Recording";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.trayIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Desktop Recording Utility";
+            this.trayIcon.Visible = true;
+            // 
+            // desktopSelector
+            // 
+            this.desktopSelector.DropDown = this.desktopSelectorStrip;
+            this.desktopSelector.Name = "desktopSelector";
+            this.desktopSelector.Size = new System.Drawing.Size(155, 22);
+            this.desktopSelector.Text = "Desktop Selector";
+            // 
+            // desktopSelectorStrip
+            // 
+            this.desktopSelectorStrip.Name = "desktopSelectorStrip";
+            this.desktopSelectorStrip.Size = new System.Drawing.Size(61, 4);
+            this.desktopSelectorStrip.Opening += new System.ComponentModel.CancelEventHandler(this.desktopSelectorStrip_Opening);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(152, 6);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.exitMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startRecording;
+        private System.Windows.Forms.ToolStripMenuItem stopRecording;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ToolStripMenuItem desktopSelector;
+        private System.Windows.Forms.ContextMenuStrip desktopSelectorStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
     }
 }
 
--- GrabThatDesktop/Form1.cs
+++ GrabThatDesktop/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,41 @@ namespace GrabThatDesktop
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Size = new Size(0, 0);
+            Visible = false;
+
+            desktopSelectorStrip.Items.Clear();
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                string display_number = Regex.Match(screen.DeviceName, @"\d+").Value;
+                ToolStripMenuItem item = (ToolStripMenuItem) desktopSelectorStrip.Items.Add(display_number);
+                if (screen.Primary)
+                {
+                    item.Checked = true;
+                }
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void desktopSelectorStrip_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem == exitMenuItem)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
         }
     }
 }
