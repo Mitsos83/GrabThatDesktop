3c04ca91358d6433a0569c212d1daf1313fd554f
Mitsos83 <saougos@hotmail.com>
2021-10-03 22:59:44 +0300

    second iteration

--- GrabThatDesktop/Form1.Designer.cs
+++ GrabThatDesktop/Form1.Designer.cs
@@ -31,14 +31,15 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.startRecording = new System.Windows.Forms.ToolStripMenuItem();
-            this.stopRecording = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.desktopSelector = new System.Windows.Forms.ToolStripMenuItem();
             this.desktopSelectorStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.stopRecording = new System.Windows.Forms.ToolStripMenuItem();
+            this.startRecording = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,25 +55,50 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
             this.contextMenuStrip1.ShowImageMargin = false;
-            this.contextMenuStrip1.Size = new System.Drawing.Size(156, 126);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(138, 104);
             this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
             // 
-            // startRecording
+            // desktopSelector
             // 
-            this.startRecording.Name = "startRecording";
-            this.startRecording.Size = new System.Drawing.Size(155, 22);
-            this.startRecording.Text = "Start Recording";
+            this.desktopSelector.DropDown = this.desktopSelectorStrip;
+            this.desktopSelector.Name = "desktopSelector";
+            this.desktopSelector.Size = new System.Drawing.Size(137, 22);
+            this.desktopSelector.Text = "Desktop Selector";
             // 
-            // stopRecording
+            // desktopSelectorStrip
             // 
-            this.stopRecording.Name = "stopRecording";
-            this.stopRecording.Size = new System.Drawing.Size(155, 22);
-            this.stopRecording.Text = "Stop Recording";
+            this.desktopSelectorStrip.Name = "desktopSelectorStrip";
+            this.desktopSelectorStrip.Size = new System.Drawing.Size(181, 26);
+            this.desktopSelectorStrip.Opening += new System.ComponentModel.CancelEventHandler(this.desktopSelectorStrip_Opening);
+            this.desktopSelectorStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.desktopSelectorStrip_ItemClicked);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(134, 6);
+            // 
+            // stopRecording
+            // 
+            this.stopRecording.Name = "stopRecording";
+            this.stopRecording.Size = new System.Drawing.Size(137, 22);
+            this.stopRecording.Text = "Stop Recording";
+            // 
+            // startRecording
+            // 
+            this.startRecording.Name = "startRecording";
+            this.startRecording.Size = new System.Drawing.Size(137, 22);
+            this.startRecording.Text = "Start Recording";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(134, 6);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.exitMenuItem.Text = "Exit";
             // 
             // trayIcon
             // 
@@ -82,37 +108,29 @@
             this.trayIcon.Text = "Desktop Recording Utility";
             this.trayIcon.Visible = true;
             // 
-            // desktopSelector
+            // saveFileDialog1
             // 
-            this.desktopSelector.DropDown = this.desktopSelectorStrip;
-            this.desktopSelector.Name = "desktopSelector";
-            this.desktopSelector.Size = new System.Drawing.Size(155, 22);
-            this.desktopSelector.Text = "Desktop Selector";
-            // 
-            // desktopSelectorStrip
-            // 
-            this.desktopSelectorStrip.Name = "desktopSelectorStrip";
-            this.desktopSelectorStrip.Size = new System.Drawing.Size(61, 4);
-            this.desktopSelectorStrip.Opening += new System.ComponentModel.CancelEventHandler(this.desktopSelectorStrip_Opening);
-            // 
-            // toolStripSeparator2
-            // 
-            this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(152, 6);
-            // 
-            // exitMenuItem
-            // 
-            this.exitMenuItem.Name = "exitMenuItem";
-            this.exitMenuItem.Size = new System.Drawing.Size(155, 22);
-            this.exitMenuItem.Text = "Exit";
+            this.saveFileDialog1.DefaultExt = "mp4";
+            this.saveFileDialog1.Title = "Save Video File";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.contextMenuStrip1.ResumeLayout(false);
@@ -131,6 +149,7 @@
         private System.Windows.Forms.ContextMenuStrip desktopSelectorStrip;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- GrabThatDesktop/Form1.cs
+++ GrabThatDesktop/Form1.cs
@@ -1,48 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
+using SharpAvi;
+using SharpAvi.Codecs;
+using SharpAvi.Output;
+
+
 
 namespace GrabThatDesktop
 {
     public partial class Form1 : Form
     {
+        private bool m_isRecording;
+        private List<ToolStripMenuItem> screens;
+        private int m_selectedScreenIndex;
+        private DimmedForm m_dimmed;
+        private Captura.Recorder m_recorder;
+        private String m_tempFile;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void updateContextStrip()
+        {
+            if (m_isRecording)
+            {
+                contextMenuStrip1.Items[2].Enabled = true;
+                contextMenuStrip1.Items[3].Enabled = false;
+            }
+            else
+            {
+                contextMenuStrip1.Items[2].Enabled = false;
+                contextMenuStrip1.Items[3].Enabled = true;
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            screens = new List<ToolStripMenuItem>();
+
             Size = new Size(0, 0);
             Visible = false;
+            m_isRecording = false;
+            m_selectedScreenIndex = -1;
 
             desktopSelectorStrip.Items.Clear();
             foreach (Screen screen in Screen.AllScreens)
             {
                 string display_number = Regex.Match(screen.DeviceName, @"\d+").Value;
                 ToolStripMenuItem item = (ToolStripMenuItem) desktopSelectorStrip.Items.Add(display_number);
+                screens.Add(item);
                 if (screen.Primary)
                 {
                     item.Checked = true;
+                    m_selectedScreenIndex = screens.Count - 1;
                 }
             }
+
+            updateContextStrip();
+
+            m_dimmed = new DimmedForm();
+        }
+
+        private void ShowSelectedScreen()
+        {
+            if (m_selectedScreenIndex < 0 || m_selectedScreenIndex > screens.Count - 1)
+            {
+                return;
+            }
+
+            m_dimmed.Hide();
+
+            m_dimmed.Bounds = Screen.AllScreens[m_selectedScreenIndex].Bounds;
+            m_dimmed.Location = Screen.AllScreens[m_selectedScreenIndex].WorkingArea.Location;
+            m_dimmed.Show();
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
             Hide();
+            ShowSelectedScreen();
         }
 
         private void desktopSelectorStrip_Opening(object sender, CancelEventArgs e)
         {
 
+        }
+
+        private void StartRecording()
+        {
+            m_isRecording = true;
+            updateContextStrip();
+            m_dimmed.Hide();
+
+            if (m_recorder != null)
+            {
+                m_recorder.Dispose();
+                m_recorder = null;
+            }
+
+            m_tempFile = Path.GetTempFileName();
+
+            m_recorder = new Captura.Recorder(new Captura.RecorderParams(m_tempFile, 10, SharpAvi.KnownFourCCs.Codecs.X264, 100, m_selectedScreenIndex));
+
+        }
+
+        private void StopRecording()
+        {
+            m_isRecording = false;
+            updateContextStrip();
+            m_dimmed.Show();
+
+            m_recorder.Dispose();
+            m_recorder = null;
+
+            saveFileDialog1.ShowDialog();
         }
 
         private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -51,6 +134,189 @@ namespace GrabThatDesktop
             {
                 System.Windows.Forms.Application.Exit();
             }
+
+            if (e.ClickedItem == startRecording)
+            {
+                StartRecording();
+                return;
+            }
+
+            if (e.ClickedItem == stopRecording)
+            {
+                StopRecording();
+                return;
+            }
+        }
+
+        private void desktopSelectorStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ToolStripMenuItem selected = screens[0];
+            foreach(ToolStripMenuItem item in screens)
+            {
+                item.Checked = false;
+                if (e.ClickedItem == item)
+                {
+                    selected = item;
+                }
+            }
+
+            selected.Checked = true;
+            Int32.TryParse(selected.Text.Trim(), out m_selectedScreenIndex);
+            m_selectedScreenIndex--;
+
+            ShowSelectedScreen();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            File.Copy(m_tempFile, saveFileDialog1.FileName, true);
+            File.Delete(m_tempFile);
         }
     }
+
+    namespace Captura
+    {
+        // Used to Configure the Recorder
+        public class RecorderParams
+        {
+            public RecorderParams(string filename, int FrameRate, FourCC Encoder, int Quality, int ScreenIndex)
+            {
+                FileName = filename;
+                FramesPerSecond = FrameRate;
+                Codec = Encoder;
+                this.Quality = Quality;
+
+                Location = Screen.AllScreens[ScreenIndex].Bounds.Location;
+                Height = Screen.AllScreens[ScreenIndex].Bounds.Height;
+                Width = Screen.AllScreens[ScreenIndex].Bounds.Width;
+            }
+
+            string FileName;
+            public int FramesPerSecond, Quality;
+            FourCC Codec;
+
+            public Point Location { get; private set; }
+            public int Height { get; private set; }
+            public int Width { get; private set; }
+
+            public AviWriter CreateAviWriter()
+            {
+                return new AviWriter(FileName)
+                {
+                    FramesPerSecond = FramesPerSecond,
+                    EmitIndex1 = true,
+                };
+            }
+
+            public IAviVideoStream CreateVideoStream(AviWriter writer)
+            {
+                // Select encoder type based on FOURCC of codec
+                if (Codec == KnownFourCCs.Codecs.Uncompressed)
+                    return writer.AddUncompressedVideoStream(Width, Height);
+                else if (Codec == KnownFourCCs.Codecs.MotionJpeg)
+                    return writer.AddMotionJpegVideoStream(Width, Height, Quality);
+                else
+                {
+                    return writer.AddMpeg4VideoStream(Width, Height, (double)writer.FramesPerSecond,
+                        // It seems that all tested MPEG-4 VfW codecs ignore the quality affecting parameters passed through VfW API
+                        // They only respect the settings from their own configuration dialogs, and Mpeg4VideoEncoder currently has no support for this
+                        quality: Quality,
+                        codec: Codec,
+                        // Most of VfW codecs expect single-threaded use, so we wrap this encoder to special wrapper
+                        // Thus all calls to the encoder (including its instantiation) will be invoked on a single thread although encoding (and writing) is performed asynchronously
+                        forceSingleThreadedAccess: true);
+                }
+            }
+        }
+
+        public class Recorder : IDisposable
+        {
+            #region Fields
+            AviWriter writer;
+            RecorderParams Params;
+            IAviVideoStream videoStream;
+            Thread screenThread;
+            ManualResetEvent stopThread = new ManualResetEvent(false);
+            #endregion
+
+            public Recorder(RecorderParams Params)
+            {
+                this.Params = Params;
+
+                // Create AVI writer and specify FPS
+                writer = Params.CreateAviWriter();
+
+                // Create video stream
+                videoStream = Params.CreateVideoStream(writer);
+                // Set only name. Other properties were when creating stream, 
+                // either explicitly by arguments or implicitly by the encoder used
+                videoStream.Name = "Captura";
+
+                screenThread = new Thread(RecordScreen)
+                {
+                    Name = typeof(Recorder).Name + ".RecordScreen",
+                    IsBackground = true
+                };
+
+                screenThread.Start();
+            }
+
+            public void Dispose()
+            {
+                stopThread.Set();
+                screenThread.Join();
+
+                // Close writer: the remaining data is written to a file and file is closed
+                writer.Close();
+
+                stopThread.Dispose();
+            }
+
+            void RecordScreen()
+            {
+                var frameInterval = TimeSpan.FromSeconds(1 / (double)writer.FramesPerSecond);
+                var buffer = new byte[Params.Width * Params.Height * 4];
+                Task videoWriteTask = null;
+                var timeTillNextFrame = TimeSpan.Zero;
+
+                while (!stopThread.WaitOne(timeTillNextFrame))
+                {
+                    var timestamp = DateTime.Now;
+
+                    Screenshot(buffer);
+
+                    // Wait for the previous frame is written
+                    videoWriteTask?.Wait();
+
+                    // Start asynchronous (encoding and) writing of the new frame
+                    videoWriteTask = videoStream.WriteFrameAsync(true, buffer, 0, buffer.Length);
+
+                    timeTillNextFrame = timestamp + frameInterval - DateTime.Now;
+                    if (timeTillNextFrame < TimeSpan.Zero)
+                        timeTillNextFrame = TimeSpan.Zero;
+                }
+
+                // Wait for the last frame is written
+                videoWriteTask?.Wait();
+            }
+
+            public void Screenshot(byte[] Buffer)
+            {
+                using (var BMP = new Bitmap(Params.Width, Params.Height))
+                {
+                    using (var g = Graphics.FromImage(BMP))
+                    {
+                        g.CopyFromScreen(Params.Location, new Point(0,0), new Size(Params.Width, Params.Height), CopyPixelOperation.SourceCopy);
+
+                        g.Flush();
+
+                        var bits = BMP.LockBits(new Rectangle(0, 0, Params.Width, Params.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                        Marshal.Copy(bits.Scan0, Buffer, 0, Buffer.Length);
+                        BMP.UnlockBits(bits);
+                    }
+                }
+            }
+        }
+    }
+
 }
